52f8bb27b26f2b602d64ee34bb853d2ee219990d
Adeola <inboxadeola@gmail.com>
2020-10-12 15:07:39 +0200

    variable renaming

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 
-""" 
-@author Kola-Olaleye Adeola
 
-"""
+# @author Kola-Olaleye Adeola
+#
+#
+
 
 import sys
 import pygame
@@ -10,17 +11,14 @@ import math
 from queue import PriorityQueue
 
 # initializing pygame
-
 pygame.init()
 
 # set the height, width and caption of the actaul screen
-
 size = width, height = 1000, 1000
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Path Finding Visualization")
 
 # a dict that contains all the colors used to denote the current state of each node
-
 colors = {"red": (255, 0, 0), "green": (0, 255, 0),  "black": (0, 0, 0), "purple": (128, 0, 128),  "blue": (
     0, 255, 0), "yellow": (255, 255, 0), "white": (255, 255, 255), "orange": (255, 165, 0),  "grey": (128, 128, 128), "turquoise": (64, 224, 208), }
 
@@ -33,7 +31,6 @@ class Node:
     # since the class can be called multiple times each Node will have it's own unique properties
 
     def __init__(self, rowOfNode,  colOfNode, widthOfNode, totalRows):
-
         self.rowOfNode = rowOfNode
         self.colOfNode = colOfNode
         self.xAxis = rowOfNode * widthOfNode
@@ -41,7 +38,7 @@ class Node:
         self.widthOfNode = widthOfNode
         self.totalRows = totalRows
         self.initColor = colors["white"]
-        self.others = []
+        self.neighbourNodes = []
 
     # color denotion
     # red denotes a Node that has been looked at
@@ -49,58 +46,59 @@ class Node:
     # black denotes a Node that cannot be visited
     # orange denotes a Node that has the start privilege
     # blue denotes a Node that has the end privilege
-    # white denotes a Node that has been reset. Meaning the Node still has it's initial Color
+    # white denotes a Node that has been reset. Meaning the Node has it's initial Color
 
     def getPosition(self):
-
         return self.rowOfNode, self.colOfNode
 
     def isNodeClose(self):
-
         return self.initColor == colors["red"]
 
     def isNodeOpen(self):
-
         return self.initColor == colors["green"]
 
     def isNodeBlocked(self):
-
         return self.initColor == colors["black"]
 
     def isNodeStart(self):
-
         return self.initColor == colors["blue"]
 
     def isNodeEnd(self):
+        return self.initColor == colors["turquoise"]
 
-        return self.initColor == colors["purple"]
-
-    def isNodeReset(self):
-
-        return self.initColor == colors["white"]
+    def resetNodeColor(self):
+        self.initColor == colors["white"]
 
     # this set of functions below are responsible int assigning colors to each Node
 
     def makeNodeClose(self):
-
-        return self.initColor == colors["red"]
+        self.initColor == colors["red"]
 
     def makeNodeOpen(self):
-
-        return self.initColor == colors["green"]
+        self.initColor == colors["green"]
 
     def makeNodeBlocked(self):
+        self.initColor == colors["black"]
 
-        return self.initColor == colors["black"]
-
-    def makeNodeStart(self):
-
-        return self.initColor == colors["blue"]
+    # def makeNodeStart(self):
+    #     self.initColor == colors["blue"]
 
     def makeNodeEnd(self):
+        self.initColor == colors["turquoise"]
 
-        return self.initColor == colors["purple"]
+    def makePath(self):
+        self.initColor == colors["purple"]
 
-    def makeNodeReset(self):
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.initColor, (self.xAxis,
+                                                  self.yAxis, self.widthOfNode, self.widthOfNode))
 
-        return self.initColor == colors["white"]
+    def draw(self, screen):
+
+        pass
+
+    def updateNeighbourNodes(self, grid):
+        pass
+
+    def __lt__(self, value):
+        return False
